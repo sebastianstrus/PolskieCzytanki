2cd192784e96f1b0e29e3d190bc2c7f4b987fa99
Sebastian Strus <sebastian.strus1@gmail.com>
2026-05-26 11:35:25 +0200

    Initial Commit

---
+++ Polskie Czytanki/Polskie_CzytankiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Polskie_CzytankiApp.swift
+//  Polskie Czytanki
+//
+//  Created by Sebastian Strus on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct Polskie_CzytankiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
